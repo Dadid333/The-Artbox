282de3e68f6da67b062292b135f7af1709893a1a
Dadid333 <dadid333@gmail.com>
2023-05-12 17:28:45 +0200

    factorisation ok

---
+++ oeuvre.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>The ArtBox</title>
+</head>
+<body>
+<header>
+    <?php include_once('header.php'); ?>
+</header>
+<main>
+<?php include_once('oeuvres.php'); ?>
+    <?php foreach($works as $work) : ?>
+        <?php if (array_key_exists('id', $work) && $work['id'] == $_GET['id']): ?>
+            <article id="detail-oeuvre">
+                <div id="img-oeuvre">
+                    <img src="<?php echo $work['image']; ?>" alt="<?php echo $work['title']; ?>">
+                </div>
+                <div id="contenu-oeuvre">
+                    <h1><?php echo $work['title']; ?></h1>
+                    <p class="description"><?php echo $work['author']; ?></p>
+                    <p class="description-complete"><?php echo $work['description']; ?></p>
+                </div>
+            </article>
+        <?php endif ?>
+    <?php endforeach ?>
+</main>
+<footer>
+    <?php include_once('footer.php'); ?>
+</footer>
+</body>
+</html>
--- oeuvres.php
+++ oeuvres.php
@@ -3,6 +3,7 @@
 $works = [
     [
         'id' => 1,
+        'link' => 'oeuvre-1.php',
         'image' => 'img/clark-van-der-beken.png',
         'title' => 'Dodomu',
         'author' => 'Mia Tozerski',
@@ -11,6 +12,7 @@ $works = [
     ],
     [
         'id' => 2,
+        'link' => 'oeuvre-2.php',
         'image' => 'img/pawel-czerwinski-3.png',
         'title' => 'Aashaaheen Baadal',
         'author' => 'Anaisha Devi',
@@ -20,6 +22,7 @@ $works = [
     [
         'id' => 3,
         'image' => 'img/dan-cristian-padure.png',
+        'link' => 'oeuvre-3.php',
         'title' => 'Nightlife Traffic',
         'author' => 'Andrew Forsythe',
         'description' => '                Quisque accumsan ultrices ligula vestibulum posuere. Aliquam feugiat ligula eget massa blandit condimentum. Morbi volutpat erat luctus suscipit pellentesque. Quisque cursus tempor nibh at sollicitudin. Sed blandit libero velit. Etiam tincidunt facilisis mollis. Ut mollis nunc sit amet lacinia luctus. Suspendisse volutpat enim semper arcu rutrum, et iaculis risus interdum. Duis at libero.
@@ -28,6 +31,7 @@ $works = [
     [
         'id' => 4,
         'image' => 'img/steve-johnson-5.png',
+        'link' => 'oeuvre-4.php',
         'title' => 'Le refuge de l\'Havre',
         'author' => 'Simon Pelletier',
         'description' => '                Nam tempus neque nec felis venenatis auctor. Nam velit risus, lobortis eu quam non, interdum efficitur nibh. Phasellus a augue ac orci lacinia mattis et vel lectus. Sed nec tellus urna. Donec at turpis turpis. Cras quam tellus, imperdiet vitae finibus id, varius quis felis. Maecenas blandit eleifend risus, vel hendrerit erat dignissim id. Nullam at laoreet nibh. Nulla gravida varius sollicitudin. Etiam non aliquam diam, tempor varius sapien. Aenean et velit eu nisi lobortis massa nunc.
@@ -36,6 +40,7 @@ $works = [
     [
         'id' => 5,
         'image' => 'img/steve-johnson.png',
+        'link' => 'oeuvre-5.php',
         'title' => 'Red Washover',
         'author' => 'Kit Van Der Borght',
         'description' => '                Nunc euismod ullamcorper tortor, id efficitur ante interdum in. Integer eu condimentum nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras viverra suscipit feugiat. Mauris vehicula luctus tellus, eu hendrerit libero laoreet ut. In tristique vehicula nisl in tempus. Morbi tempus aliquet gravida. In eget est congue, rhoncus sapien at, cursus metus.
@@ -44,6 +49,7 @@ $works = [
     [
         'id' => 6,
         'image' => 'img/pawel-czerwinski.png',
+        'link' => 'oeuvre-6.php',
         'title' => 'Chromatics',
         'author' => 'Jean-Michel Delatronchette',
         'description' => '                Vivamus commodo non libero at hendrerit. In lacinia dui sit amet pellentesque iaculis. Donec at ultricies sem porttitor.
@@ -52,6 +58,7 @@ $works = [
     [
         'id' => 7,
         'image' => 'img/jazmin-quaynor.png',
+        'link' => 'oeuvre-7.php',
         'title' => 'Digital Negative',
         'author' => 'Hamish McKee',
         'description' => '                Integer in nisl posuere, pulvinar ex ac, tincidunt risus. Nullam vel lorem et leo dignissim accumsan. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempor, magna non consectetur dapibus, est libero iaculis lacus, eget semper turpis orci vitae felis. Fusce eget molestie.
@@ -60,6 +67,7 @@ $works = [
     [
         'id' => 8,
         'image' => 'img/steve-johnson-6.png',
+        'link' => 'oeuvre-8.php',
         'title' => 'Blast from the past',
         'author' => 'Juliette Baskerville',
         'description' => '                Nunc fermentum purus dapibus justo fermentum auctor. Maecenas non tincidunt leo. Morbi vitae iaculis sem. Donec quis scelerisque massa. Fusce quis accumsan diam, et interdum lectus. Suspendisse mattis pulvinar vehicula. Duis nisi.
@@ -68,6 +76,7 @@ $works = [
     [
         'id' => 9,
         'image' => 'img/victor-grabarczyk.png',
+        'link' => 'oeuvre-9.php',
         'title' => 'Hurricane',
         'author' => 'Natalie Wellington',
         'description' => '                Aliquam tristique tempus molestie. Nulla nisl eros, dapibus eu lectus in, cursus accumsan arcu. Suspendisse bibendum diam dignissim porta maximus. Praesent sollicitudin consectetur faucibus. Cras pulvinar massa a orci rutrum, id blandit enim viverra. Praesent sed congue augue. Suspendisse efficitur, nisl quis finibus faucibus, lacus felis bibendum leo, eu euismod lacus mauris in felis. Quisque dignissim et dui et aliquet. Donec ut lobortis eros, vitae tincidunt augue metus.
@@ -76,6 +85,7 @@ $works = [
     [
         'id' => 10,
         'image' => 'img/pawel-czerwinski-2.png',
+        'link' => 'oeuvre-10.php',
         'title' => 'La marée rouge',
         'author' => 'Martin Rodriguez',
         'description' => '                Vivamus quis odio vel ligula feugiat facilisis. Donec eleifend pellentesque massa, ut malesuada est bibendum sit amet. Morbi tincidunt nec tellus vel ornare. Mauris dolor tellus, gravida eget euismod eu, viverra eget urna. Phasellus feugiat ipsum nec lorem accumsan, sed porta quam dictum massa nunc.
@@ -84,6 +94,7 @@ $works = [
     [
         'id' => 11,
         'image' => 'img/steve-johnson-2.png',
+        'link' => 'oeuvre-11.php',
         'title' => 'Asimilacion',
         'author' => 'Angel Sanchez-Fernandez',
         'description' => '                Mauris ut justo ac mi pretium eleifend. Curabitur sed magna ut elit facilisis pharetra. Maecenas tincidunt fermentum ipsum ut sollicitudin. Nullam feugiat, neque vel egestas sollicitudin, quam leo mattis mauris, in lacinia sem mi id risus. Nullam ultrices quam eu leo auctor tempus. Fusce vestibulum mi ex, vel ultricies purus mollis sollicitudin. Aenean ac vehicula ipsum. Nam turpis ante, ultrices eget odio sed, luctus bibendum mauris sodales sed.
@@ -92,6 +103,7 @@ $works = [
     [
         'id' => 12,
         'image' => 'img/fly-d.png',
+        'link' => 'oeuvre-12.php',
         'title' => 'La Galaxia Gialla',
         'author' => 'Eduardo Tancredi',
         'description' => '                Mauris maximus, orci sollicitudin ultrices elementum, tellus neque feugiat leo, quis lobortis purus neque vel lectus. Ut sagittis eros id lectus porttitor tincidunt. Donec scelerisque diam nec felis egestas, eget finibus ante porttitor. Sed malesuada sapien ut semper accumsan. Duis tristique dui vel egestas porttitor. Nunc tristique dapibus orci a amet.
@@ -100,6 +112,7 @@ $works = [
     [
         'id' => 13,
         'image' => 'img/orfeas-green.png',
+        'link' => 'oeuvre-13.php',
         'title' => 'Puffy Amalgamate',
         'author' => 'Sandro De Blasi',
         'description' => '                Donec semper a massa quis congue. In malesuada lorem ligula, ut posuere magna pulvinar in. Proin vitae enim gravida, commodo odio.
@@ -108,6 +121,7 @@ $works = [
     [
         'id' => 14,
         'image' => 'img/steve-johnson-4.png',
+        'link' => 'oeuvre-14.php',
         'title' => 'Mirage',
         'author' => 'Stéphanie Kaiser',
         'description' => '                Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam iaculis lorem ac ex tristique egestas et nec sapien. Donec tincidunt id erat sit amet tempus. Nullam vel molestie dui. Duis a neque massa. Vivamus quis ornare lacus. Nullam eleifend condimentum egestas. Vivamus magna purus, fermentum mollis mauris sed, consectetur interdum libero. Duis interdum tortor tellus, eget sollicitudin quis.
@@ -116,15 +130,12 @@ $works = [
     [
         'id' => 15,
         'image' => 'img/steve-johnson-3.png',
+        'link' => 'oeuvre-15.php',
         'title' => 'Blaue Gelbe Muster',
         'author' => 'Adelheid Von Schreiber',
         'description' => '                Curabitur dui odio, porta vel tempor sed, consectetur vitae mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Orci varius natoque penatibus nec.
         ', 
     ],
 ];
-
-echo '<pre>';
-print_r ($works);
-echo '<pre>';
 
 ?>
