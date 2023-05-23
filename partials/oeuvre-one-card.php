a3d43245e9c1e4832a479965a5e845a9762f780c
Dadid333 <dadid333@gmail.com>
2023-05-23 10:39:08 +0200

    modif suite mentorat 2

---
+++ partials/oeuvre-one-card.php
@@ -0,0 +1,13 @@
+<?php include_once('oeuvres.php');
+foreach($works as $work) : ?>
+<article id="detail-oeuvre">
+    <div id="img-oeuvre">
+        <img src="<?= $work['image']; ?>" alt="<?= $work['title']; ?>">
+    </div>
+    <div id="contenu-oeuvre">
+        <h1><?= $work['title']; ?></h1>
+        <p class="description"><?= $work['author']; ?></p>
+        <p class="description-complete"><?= $work['description']; ?></p>
+    </div>
+    <?php endforeach ?>
+</article>
