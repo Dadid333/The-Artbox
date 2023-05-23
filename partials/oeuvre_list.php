0b72c29b8d2df9ef27cdfc6d594369bd13e29d1f
Dadid333 <dadid333@gmail.com>
2023-05-23 10:58:18 +0200

    simplification code oeuvre_list

--- partials/oeuvre_list.php
+++ partials/oeuvre_list.php
@@ -1,9 +1,9 @@
-<?php if ($work['is_enabled'] == true) : ?>
+<?php if ($work['is_enabled']) : ?>
     <article class="oeuvre">
-        <a href="oeuvre.php?id=<?php echo $work['id']; ?>">
-            <img src="<?php echo $work['image']; ?>" alt="<?php echo $work['title']; ?>">
-            <h2><?php echo $work['title']; ?></h2>
-            <p class="description"><?php echo $work['author']; ?></p>
+        <a href="oeuvre.php?id=<?= $work['id']; ?>">
+            <img src="<?= $work['image']; ?>" alt="<?= $work['title']; ?>">
+            <h2><?= $work['title']; ?></h2>
+            <p class="description"><?= $work['author']; ?></p>
         </a>
     </article>                               
 <?php endif ?>
