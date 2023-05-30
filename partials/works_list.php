b58d073b1b34085bf42bc13ea06b3c2df2c8d3ef
Dadid333 <dadid333@gmail.com>
2023-05-30 09:10:29 +0200

    modif suite à soutenance

--- partials/works_list.php
+++ partials/works_list.php
@@ -1,17 +1,16 @@
+<main>
 <div id="liste-oeuvres">          
-<?php include_once('oeuvres1.php'); ?>
-<?php foreach ($works as $value) {
-    // Récupérer les données de l'élément
-    $work = current($value);
-    // Vérifier si l'œuvre est activée
+<?php include_once('oeuvres.php'); ?>
+<?php foreach ($works as $work) {
     if ($work['is_enabled']) { ?>
-    <article class="oeuvre">
-        <a href="oeuvre.php?id=<?= $work['id']; ?>">
-            <img src="<?= $work['image']; ?>" alt="<?= $work['title']; ?>">
-            <h2><?= $work['title']; ?></h2>
-            <p class="description"><?= $work['author']; ?></p>
-        </a>
-    </article> 
+        <article class="oeuvre">
+            <a href="oeuvre.php?id=<?= $work['id']; ?>">
+                <img src="<?= $work['image']; ?>" alt="<?= $work['title']; ?>">
+                <h2><?= $work['title']; ?></h2>
+                <p class="description"><?= $work['author']; ?></p>
+            </a>
+        </article> 
 <?php }
 } ?>
 </div>
+</main>
